157a0c04704d38b436ffe7b1bf43905b09989f12
Dell Di <875372314@qq.com>
2024-08-30 17:39:43 +0800

    ZeroPadding 失败

--- src/plugins/0-swgger.ts
+++ src/plugins/0-swgger.ts
@@ -6,19 +6,19 @@ export default fp(async (fastify) => {
   await fastify.register(swagger, {
     swagger: {
       info: {
-        title: 'swagger-DellDi',
-        description: 'Testing the Fastify swagger API',
-        version: '0.1.0'
+        title: 'api_reference',
+        description: 'swagger API',
+        version: '0.1.0',
       },
       externalDocs: {
         url: 'https://swagger.io',
-        description: 'Find more info here'
+        description: 'Find more info here',
       },
       host: 'localhost',
       schemes: ['http'],
       consumes: ['application/json'],
-      produces: ['application/json']
-    }
+      produces: ['application/json'],
+    },
   })
 
   await fastify.register(swaggerUi, {
