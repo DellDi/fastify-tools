9437df82d4666fd03225de8328b6b7b0bad101b5
Dell Di <875372314@qq.com>
2024-10-11 17:49:06 +0800

    💄(style): 调整主题色、调整主页页面布局

--- packages/shadcn-next/app/(main)/dashboard/page.tsx
+++ packages/shadcn-next/app/(main)/dashboard/page.tsx
@@ -5,35 +5,47 @@ import { Card1 } from "@/app/(main)/dashboard/components/card-1";
 import { Card2 } from "@/app/(main)/dashboard/components/card-2";
 import { ProgressCards } from "@/app/(main)/dashboard/components/progress";
 import { TimeCards } from "@/app/(main)/dashboard/components/time-cards";
+import { ShowCloud } from "@/components/custom/ShowCloud";
+import { PanelCharts } from "@/components/custom/PanelCharts";
 
 export const metadata: Metadata = {
-    title: "Dashboard",
-    description: "Example dashboard app built using the components.",
+  title: "Dashboard",
+  description: "Example dashboard app built using the components.",
 }
 
 export default function DashboardPage() {
-    return (
-        <main className="flex-1 space-y-4 p-8 pt-6">
-            <div className="flex items-center justify-between space-y-2">
-                <h2 className="text-3xl font-bold tracking-tight">统计分析</h2>
-                <div className="flex items-center space-x-2">
-                    <CalendarDateRangePicker/>
-                    <Button>下载</Button>
-                </div>
-            </div>
-            <div
-                className="chart-wrapper mx-auto flex max-w-6xl flex-col flex-wrap items-start justify-center gap-6 sm:flex-row sm:p-1">
-                <div className="grid w-full gap-6 sm:grid-cols-2 lg:max-w-[26rem] lg:grid-cols-1 xl:max-w-[30rem]">
-                    <Card1/>
-                    <Card2/>
-                </div>
-                <div className="grid w-full flex-1 gap-6 lg:max-w-[24rem]">
-                    <ProgressCards/>
-                </div>
-                <div className="grid w-full flex-1 gap-6">
-                    <TimeCards/>
-                </div>
-            </div>
-        </main>
-    )
+  return (
+      <main className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex items-center justify-between space-y-2">
+          <div className="flex items-center space-x-2">
+            <CalendarDateRangePicker/>
+            <Button size="sm">下载</Button>
+          </div>
+        </div>
+        <div className="grid w-full flex-1 gap-4">
+          <PanelCharts/>
+        </div>
+        {/*max-w-6xl*/}
+        <div
+            className="chart-wrapper mx-auto flex  flex-col flex-wrap items-start justify-center gap-4 sm:flex-row sm:p-1">
+          {/*lg:max-w-[18rem] xl:max-w-[20rem]*/}
+          <div className="grid lg:max-w-[20rem] w-full gap-4 sm:grid-cols-2  lg:grid-cols-1">
+            <Card1/>
+            <Card2/>
+          </div>
+          {/*lg:max-w-[24rem]*/}
+          <div className="grid w-full flex-1 gap-4">
+            <ProgressCards/>
+          </div>
+          <div className="grid w-full flex-1 gap-4">
+            <TimeCards/>
+          </div>
+          <div className="grid w-full flex-1 gap-4">
+            <ShowCloud/>
+            <ShowCloud/>
+          </div>
+
+        </div>
+      </main>
+  )
 }
--- packages/shadcn-next/components/app-sidebar.tsx
+++ packages/shadcn-next/components/app-sidebar.tsx
@@ -1,243 +1,53 @@
 "use client"
 
-import {
-    Bird,
-    BookOpen,
-    Bot,
-    Code2,
-    Eclipse,
-    Frame,
-    History,
-    Map,
-    PieChart,
-    Rabbit,
-    Settings2,
-    SnailIcon,
-    SquareTerminal,
-    Turtle,
-} from "lucide-react"
-
 import { NavMain } from "@/components/nav-main"
 import { NavProjects } from "@/components/nav-projects"
 import { NavUser } from "@/components/nav-user"
 import { StorageCard } from "@/components/storage-card"
 import { TeamSwitcher } from "@/components/team-switcher"
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarHeader,
-    SidebarItem,
-    SidebarLabel,
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarItem,
+  SidebarLabel,
 } from "@/components/ui/sidebar"
-
-const data = {
-    teams: [
-        {
-            name: "Dell DI Home",
-            logo: SnailIcon,
-            plan: "Enterprise",
-        },
-        {
-            name: "Dell DI Corp",
-            logo: Eclipse,
-            plan: "Startup",
-        },
-        {
-            name: "Dell DI Favorites",
-            logo: Rabbit,
-            plan: "Free",
-        },
-    ],
-    user: {
-        name: "delldi",
-        email: "delldi808611@outlook.com",
-        avatar: "https://avatars.githubusercontent.com/u/40460351?v=4",
-    },
-    navMain: [
-        {
-            title: "密码解析",
-            url: "#",
-            icon: SquareTerminal,
-            isActive: true,
-            items: [
-                {
-                    title: "neesee模式",
-                    url: "/password/newsee",
-                    icon: History,
-                    description: "neesee密码加解密",
-                },
-            ],
-        },
-        {
-            title: "文件系统",
-            url: "#",
-            icon: Bot,
-            items: [
-                {
-                    title: "Genesis",
-                    url: "#",
-                    icon: Rabbit,
-                    description: "Our fastest model for general use cases.",
-                },
-                {
-                    title: "Explorer",
-                    url: "#",
-                    icon: Bird,
-                    description: "Performance and speed for efficiency.",
-                },
-                {
-                    title: "Quantum",
-                    url: "#",
-                    icon: Turtle,
-                    description: "The most powerful model for complex computations.",
-                },
-            ],
-        },
-        {
-            title: "jira中心",
-            url: "#",
-            icon: BookOpen,
-            items: [
-                {
-                    title: "个人看板",
-                    url: "/jira/personal",
-                },
-                {
-                    title: "创建工单",
-                    url: "/jira/create",
-                }
-            ],
-        },
-        {
-            title: "AIGC",
-            url: "#",
-            icon: Code2,
-            items: [
-                {
-                    title: "Chat工具",
-                    url: "/aigc/chat",
-                },
-                {
-                    title: "模型调优",
-                    url: "/aigc/model",
-                },
-            ],
-        },
-        {
-            title: "Settings",
-            url: "#",
-            icon: Settings2,
-            items: [
-                {
-                    title: "General",
-                    url: "#",
-                },
-                {
-                    title: "Team",
-                    url: "#",
-                },
-                {
-                    title: "Billing",
-                    url: "#",
-                },
-                {
-                    title: "Limits",
-                    url: "#",
-                },
-            ],
-        },
-    ],
-
-    // navSecondary: [
-    //   {
-    //     title: "Support",
-    //     url: "#",
-    //     icon: LifeBuoy,
-    //   },
-    //   {
-    //     title: "Feedback",
-    //     url: "#",
-    //     icon: Send,
-    //   },
-    // ],
-    projects: [
-        {
-            name: "Design Engineering",
-            url: "#",
-            icon: Frame,
-        },
-        {
-            name: "Sales & Marketing",
-            url: "#",
-            icon: PieChart,
-        },
-        {
-            name: "Travel",
-            url: "#",
-            icon: Map,
-        },
-    ],
-    searchResults: [
-        {
-            title: "路由基础",
-            teaser:
-                "每个应用程序的骨架都是路由。本页将向您介绍 Web 路由的基本概念以及如何在 Next.js 中处理路由。",
-            url: "#",
-        },
-        {
-            title: "布局和模板",
-            teaser:
-                "特殊文件 layout.js 和 template.js 允许您创建在路由之间共享的 UI。本页将指导您如何以及何时使用这些特殊文件。",
-            url: "#",
-        },
-        {
-            title: "数据获取、缓存和重新验证",
-            teaser:
-                "数据获取是任何应用程序的核心部分。本页介绍了如何在 React 和 Next.js 中获取、缓存和重新验证数据。",
-            url: "#",
-        },
-        {
-            title: "服务器和客户端组合模式",
-            teaser:
-                "在构建 React 应用程序时，您需要考虑应用程序的哪些部分应在服务器或客户端上呈现。",
-            url: "#",
-        },
-        {
-            title: "服务器操作和变更",
-            teaser:
-                "服务器操作是异步函数，在服务器上执行。它们可以在服务器和客户端组件中使用，以处理表单提交和 Next.js 应用程序中的数据变更。",
-            url: "#",
-        },
-    ],
-}
+import { usePathname } from 'next/navigation';
+import { routesConfig } from "@/lib/route";
 
 export function AppSidebar() {
-    return (
-        <Sidebar>
-            <SidebarHeader>
-                <TeamSwitcher teams={data.teams}/>
-            </SidebarHeader>
-            <SidebarContent>
-                <SidebarItem>
-                    <SidebarLabel>Platform</SidebarLabel>
-                    <NavMain items={data.navMain} searchResults={data.searchResults}/>
-                </SidebarItem>
-                <SidebarItem>
-                    <SidebarLabel>Projects</SidebarLabel>
-                    <NavProjects projects={data.projects}/>
-                </SidebarItem>
-                {/*<SidebarItem className="mt-auto">*/}
-                {/*<SidebarLabel>Help</SidebarLabel>*/}
-                {/*<NavSecondary items={data.navSecondary} />*/}
-                {/*</SidebarItem>*/}
-                <SidebarItem>
-                    <StorageCard/>
-                </SidebarItem>
-            </SidebarContent>
-            <SidebarFooter>
-                <NavUser user={data.user}/>
-            </SidebarFooter>
-        </Sidebar>
-    )
+  const pathname = usePathname();
+  // const pathSegments = pathname.split('/').filter((segment: string) => segment);
+  // Update isActive status based on current route
+  routesConfig.navMain.forEach(navItem => {
+    navItem.isActive = navItem.items.some(item => pathname.includes(item.url))
+  })
+  return (
+      <Sidebar>
+        <SidebarHeader>
+          <TeamSwitcher teams={routesConfig.teams}/>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarItem>
+            <SidebarLabel>Platform</SidebarLabel>
+            <NavMain items={routesConfig.navMain} searchResults={routesConfig.searchResults}/>
+          </SidebarItem>
+          <SidebarItem>
+            <SidebarLabel>Projects</SidebarLabel>
+            <NavProjects projects={routesConfig.projects}/>
+          </SidebarItem>
+          {/*<SidebarItem className="mt-auto">*/}
+          {/*<SidebarLabel>Help</SidebarLabel>*/}
+          {/*<NavSecondary items={routesConfig.navSecondary} />*/}
+          {/*</SidebarItem>*/}
+          <SidebarItem>
+            <StorageCard/>
+          </SidebarItem>
+        </SidebarContent>
+        <SidebarFooter>
+          <NavUser user={routesConfig.user}/>
+        </SidebarFooter>
+      </Sidebar>
+  )
 }
