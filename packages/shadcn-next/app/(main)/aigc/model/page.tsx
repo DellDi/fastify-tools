f4beb46979e8ae1fa7f00b20a14782719eb56b97
Dell Di <875372314@qq.com>
2024-10-14 17:41:32 +0800

    ✨ (upload): 新增附件上传界面

--- packages/shadcn-next/app/(main)/aigc/model/page.tsx
+++ packages/shadcn-next/app/(main)/aigc/model/page.tsx
@@ -9,7 +9,6 @@ import {
 
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
-
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {
