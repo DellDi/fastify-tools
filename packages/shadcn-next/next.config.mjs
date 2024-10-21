ad05f88820cff20a40d2b1af07b451eebe79da66
heart <1563183174@qq.com>
2024-10-21 22:31:43 +0800

    update(Dockerfile)，新增docker构建模式和环境变量、线上逻辑优化

--- packages/shadcn-next/next.config.mjs
+++ packages/shadcn-next/next.config.mjs
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
-import { codeInspectorPlugin } from 'code-inspector-plugin';
+import { codeInspectorPlugin } from 'code-inspector-plugin'
 
 const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config, {dev, isServer}) => {
-    config.plugins.push(codeInspectorPlugin({bundler: 'webpack'}));
-    return config;
+  webpack: (config, { dev, isServer }) => {
+    config.plugins.push(codeInspectorPlugin({ bundler: 'webpack' }))
+    return config
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
